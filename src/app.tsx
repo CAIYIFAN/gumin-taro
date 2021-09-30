cfccc13b78088c6cff6cbcce7c71e54a06d288b4
Caiyifan <1657050906@qq.com>
2021-09-30 15:45:38 +0800

    feat: init

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import { Provider } from 'mobx-react'
+import React from "react";
+import { Provider } from "mobx-react";
 
-import counterStore from './store/counter'
+import counterStore from "./store/counter";
 
-import './app.less'
+import "./app.less";
 
 const store = {
-  counterStore
+  counterStore,
+};
+
+function App(props) {
+  return <Provider store={store}>{props.children}</Provider>;
 }
 
-function App () {
-  return (
-    <Provider store={store}>
-      {this.props.children}
-    </Provider>
-  )
-}
-
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,47 +1,13 @@
 import React from "react";
-import { View, Button, Text } from "@tarojs/components";
-import { observer, inject } from "mobx-react";
+import { View, Text } from "@tarojs/components";
 import "./index.less";
 
-type PageStateProps = {
-  store: {
-    counterStore: {
-      counter: number;
-      increment: Function;
-      decrement: Function;
-      incrementAsync: Function;
-    };
-  };
-};
-
-function Index(props: PageStateProps) {
-  const increment = () => {
-    const { counterStore } = props.store;
-    counterStore.increment();
-  };
-
-  const decrement = () => {
-    const { counterStore } = props.store;
-    counterStore.decrement();
-  };
-
-  const incrementAsync = () => {
-    const { counterStore } = props.store;
-    counterStore.incrementAsync();
-  };
-
-  const {
-    counterStore: { counter },
-  } = props.store;
-
+function Index(props: any) {
   return (
     <View className="index">
-      <Button onClick={increment}>+</Button>
-      <Button onClick={decrement}>-</Button>
-      <Button onClick={incrementAsync}>Add Async</Button>
-      <Text>{counter}</Text>
+      <Text style={{ width: 100, height: 100 }}>213123123</Text>
     </View>
   );
 }
 
-export default inject("store")(observer(Index));
+export default Index;
