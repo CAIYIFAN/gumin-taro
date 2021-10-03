801be0c189f5dc7345754ba16800fcab59e42d71
caiyifan5 <caiyifan5@jd.com>
2021-10-03 11:11:27 +0800

    feat: 小优化

--- dist/pages/index/index.js
+++ dist/pages/index/index.js
@@ -1,5 +1,88 @@
 (wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["pages/index/index"],{
 
+/***/ "./node_modules/@babel/runtime/helpers/esm/extends.js":
+/*!************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/extends.js ***!
+  \************************************************************/
+/*! exports provided: default */
+/*! exports used: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return _extends; });
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/objectSpread2.js":
+/*!******************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/objectSpread2.js ***!
+  \******************************************************************/
+/*! exports provided: default */
+/*! exports used: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return _objectSpread2; });
+/* harmony import */ var _defineProperty_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./defineProperty.js */ "./node_modules/@babel/runtime/helpers/esm/defineProperty.js");
+
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+
+    if (enumerableOnly) {
+      symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+      });
+    }
+
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        Object(_defineProperty_js__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
+/***/ }),
+
 /***/ "./node_modules/@tarojs/mini-runner/node_modules/babel-loader/lib/index.js!./src/pages/index/index.tsx":
 /*!****************************************************************************************************!*\
   !*** ./node_modules/@tarojs/mini-runner/node_modules/babel-loader/lib!./src/pages/index/index.tsx ***!
@@ -74,89 +157,83 @@ function Index(props, ref) {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var _Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/slicedToArray */ "./node_modules/@babel/runtime/helpers/esm/slicedToArray.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _tarojs_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @tarojs/components */ "./node_modules/@tarojs/components/mini/index.js");
-/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./index.less */ "./src/components/check/index.less");
-/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_index_less__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var _Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_objectSpread2__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/objectSpread2 */ "./node_modules/@babel/runtime/helpers/esm/objectSpread2.js");
+/* harmony import */ var _Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/slicedToArray */ "./node_modules/@babel/runtime/helpers/esm/slicedToArray.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _tarojs_components__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @tarojs/components */ "./node_modules/@tarojs/components/mini/index.js");
+/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./index.less */ "./src/components/check/index.less");
+/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_index_less__WEBPACK_IMPORTED_MODULE_5__);
+
+
 
 
 
 
 
 function Check(props) {
-  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_1__["useState"])([]),
-      _useState2 = Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])(_useState, 2),
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_3__["useState"])([]),
+      _useState2 = Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_2__[/* default */ "a"])(_useState, 2),
       data = _useState2[0],
       setData = _useState2[1];
 
-  var _useState3 = Object(react__WEBPACK_IMPORTED_MODULE_1__["useState"])(1),
-      _useState4 = Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])(_useState3, 2),
+  var _useState3 = Object(react__WEBPACK_IMPORTED_MODULE_3__["useState"])(1),
+      _useState4 = Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_slicedToArray__WEBPACK_IMPORTED_MODULE_2__[/* default */ "a"])(_useState3, 2),
       focusKey = _useState4[0],
       setFocusKey = _useState4[1];
 
   var inputHandle = function inputHandle(value) {
-    console.log('asd', value);
+    var tmp = data.concat();
 
     if (value.detail.value) {
-      var tmp = data.concat(value.detail.value);
-      setData(tmp);
-    } else {
-      var _tmp = data.concat();
-
-      _tmp.pop();
+      var _tmp = data.concat(value.detail.value);
 
       setData(_tmp);
-      setFocusKey(data.length - 1);
+    } else {
+      tmp.pop();
+      setData(tmp);
+      var key = data.length === 1 ? data.length : data.length - 1;
+      setFocusKey(key);
     }
   };
 
-  Object(react__WEBPACK_IMPORTED_MODULE_1__["useEffect"])(function () {
-    // setFocusKey(data.length)
-    console.log(111111);
+  var defaultProps = {
+    maxlength: 1,
+    onInput: inputHandle,
+    cursorSpacing: 10
+  };
 
+  var getProps = function getProps(num) {
+    var result = [];
+
+    for (var i = 0; i < num; i++) {
+      result.push(Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_objectSpread2__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"])({
+        focus: focusKey === i + 1,
+        value: data[i],
+        disabled: i + 2 > num ? false : focusKey > i + 2
+      }, defaultProps));
+    }
+
+    return result;
+  };
+
+  Object(react__WEBPACK_IMPORTED_MODULE_3__["useEffect"])(function () {
     if (data.length === focusKey) {
       setFocusKey(data.length + 1);
     }
   }, [data.length]); // console.log(data)
 
   console.log('focusKey', focusKey, data);
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* View */ "d"], {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_4__[/* View */ "d"], {
     className: "check-wrapper"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* View */ "d"], {
-    className: "check-box-default"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* Input */ "b"], {
-    maxlength: 1,
-    onInput: inputHandle,
-    cursorSpacing: 10,
-    focus: focusKey === 1,
-    value: data[0]
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* View */ "d"], {
-    className: "check-box-default"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* Input */ "b"], {
-    maxlength: 1,
-    onInput: inputHandle,
-    cursorSpacing: 10,
-    focus: focusKey === 2,
-    value: data[1]
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* View */ "d"], {
-    className: "check-box-default"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* Input */ "b"], {
-    maxlength: 1,
-    onInput: inputHandle,
-    cursorSpacing: 10,
-    focus: focusKey === 3,
-    value: data[2]
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* View */ "d"], {
-    className: "check-box-default"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_2__[/* Input */ "b"], {
-    maxlength: 1,
-    onInput: inputHandle,
-    cursorSpacing: 10,
-    focus: focusKey > 3,
-    value: data[3]
-  })));
+  }, getProps(4).map(function (item, index) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_4__[/* View */ "d"], {
+      className: "check-box-default"
+    }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement(_tarojs_components__WEBPACK_IMPORTED_MODULE_4__[/* Input */ "b"], Object(_Users_caiyifan5_learn_gumin_taro_node_modules_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])({
+      key: index
+    }, item)));
+  }));
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (Check);
--- src/components/check/index.tsx
+++ src/components/check/index.tsx
@@ -1,28 +1,44 @@
 import React, { useEffect, useState }  from 'react'
-import { View, Button, Input } from '@tarojs/components'
+import { View, Input } from '@tarojs/components'
 import './index.less'
 
 function Check(props: any) {
   const [data, setData] = useState([])
   const [focusKey, setFocusKey] = useState(1)
 
-
   const inputHandle = (value:any) => {
-    console.log('asd',value)
+    const tmp = data.concat()
     if (value.detail.value) {
       const tmp = data.concat(value.detail.value)
       setData(tmp)
     } else {
-      const tmp = data.concat()
       tmp.pop();
       setData(tmp)
-      setFocusKey(data.length - 1)
+      const key = data.length === 1 ? data.length : data.length - 1
+      setFocusKey(key)
     }
   }
 
+  const defaultProps = {
+    maxlength: 1,
+    onInput: inputHandle,
+    cursorSpacing: 10,
+  }
+
+  const getProps = (num) => {
+    const result: any = []
+    for(let i = 0; i < num; i++) {
+      result.push({
+        focus: focusKey ===  (i+1),
+        value: data[i],
+        disabled: (i + 2 ) > num ? false : focusKey > (i+2),
+        ...defaultProps,
+      })
+    }
+    return result
+  }
+
   useEffect(() => {
-    // setFocusKey(data.length)
-    console.log(111111)
     if (data.length === focusKey) {
       setFocusKey(data.length + 1)
     }
@@ -32,18 +48,11 @@ function Check(props: any) {
   console.log('focusKey', focusKey, data)
   return (
     <View className='check-wrapper'>
-      <View className='check-box-default'>
-        <Input maxlength={1} onInput={inputHandle} cursorSpacing={10} focus={focusKey === 1} value={data[0]} />
-      </View>
-      <View className='check-box-default'>
-        <Input maxlength={1} onInput={inputHandle} cursorSpacing={10} focus={focusKey === 2} value={data[1]} />
-      </View>
-      <View className='check-box-default'>
-        <Input maxlength={1} onInput={inputHandle} cursorSpacing={10} focus={focusKey === 3} value={data[2]} />
-      </View>
-      <View className='check-box-default'>
-        <Input maxlength={1} onInput={inputHandle} cursorSpacing={10} focus={focusKey > 3} value={data[3]} />
-      </View>
+      {getProps(4).map((item, index) => (
+        <View className='check-box-default'>
+          <Input key={index} {...item} />
+        </View>
+      ))}
     </View>
   );
 }
